c88d63e781a3e03f44184b1fa0711ce82afa461a
Danielle Dempsey <ddempsey@perennia.ca>
2025-06-20 14:09:31 -0300

    initial commit

---
+++ 2_generate_model_report.R
@@ -0,0 +1,27 @@
+# June 20, 2025
+
+library(here)
+library(quarto)
+
+# user defined parameters -------------------------------------------------
+
+mean_depth_m <- 14
+start_val <- 2020
+
+# export html file --------------------------------------------------------
+
+quarto::quarto_render(
+  here("2_model_template.qmd"),
+  execute_params = list(depth_m = mean_depth_m),
+  output_file = paste0(
+    paste(
+      "pockwock_temperature_model",
+      mean_depth_m,
+      start_val,
+      sep = "_"
+    ), ".html")
+)
+
+
+
+
